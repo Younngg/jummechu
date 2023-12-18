fcfc71e9317f14e659a417af5a74ffca14999a38
younngg <8533283@naver.com>
2023-12-18 19:13:53 +0900

    fix: addUser 수정

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,13 +1,7 @@
-import { AuthUser } from '@/types/user';
+import { User } from '@/types/user';
 import { client } from './sanity';
 
-export const addUser = async ({
-  id,
-  username,
-  email,
-  name,
-  image,
-}: AuthUser) => {
+export const addUser = async ({ id, username, email, name, image }: User) => {
   return client.createIfNotExists({
     _id: id,
     _type: 'user',
