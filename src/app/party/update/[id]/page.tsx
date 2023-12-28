f6c4208fa70aaee868bc396f4890cd0d4434b792
younngg <8533283@naver.com>
2023-12-28 20:35:55 +0900

    fix: import 오류 수정

--- src/app/party/update/[id]/page.tsx
+++ src/app/party/update/[id]/page.tsx
@@ -1,6 +1,5 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import authOptions from '@/app/api/auth/[...nextauth]/options';
 import PartyForm from '@/components/PartyForm';
-import { getParty } from '@/service/sanity/party';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 
