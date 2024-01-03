1e0186c6d59aba8ba4805cc39e38c2e793a53573
younngg <8533283@naver.com>
2024-01-03 19:11:28 +0900

    fix: 날짜 포맷팅

--- src/components/PartyCard.tsx
+++ src/components/PartyCard.tsx
@@ -1,16 +1,31 @@
 import { SimpleParty } from '@/types/party';
+import dayjs from 'dayjs';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
 type Props = {
   party: SimpleParty;
 };
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const PartyCard = ({
   party: { name, id, updatedAt, createdBy, isClosed },
 }: Props) => {
   const { data: session } = useSession();
   const user = session?.user;
+
+  const formatDate = () => {
+    const previous = dayjs(updatedAt).tz('Asia/Seoul');
+    const year = previous.year();
+    const month = previous.month() + 1;
+    const date = previous.date();
+
+    return `${year}-${('00' + month).slice(-2)}-${('00' + date).slice(-2)}`;
+  };
 
   return (
     <Link
@@ -23,7 +38,7 @@ const PartyCard = ({
         {name}
         {createdBy.id === user?.id && <span>👑</span>}
       </p>
-      <p>{updatedAt}</p>
+      <p>{formatDate()}</p>
     </Link>
   );
 };
